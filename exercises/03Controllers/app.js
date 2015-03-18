718f13c2e7a1e7aa405448dc4ea8901798db28d9
rasmusvhansen <rasmusvhansen@gmail.com>
2015-03-18 20:15:00 +0000

    more

--- exercises/03Controllers/app.js
+++ exercises/03Controllers/app.js
@@ -10,7 +10,7 @@
       return $http.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&type=video&q=' + ctrl.query + '&key=AIzaSyD4YJITOWdfQdFbcxHc6TgeCKmVS9yRuQ8')
         .then(function(res) {        
           $log.info(res.data.items);
-          // 1. Look in console and figure out how to map the array of items to an array of objects with: Title, Description, Link, Thumbnail
+          // 1. Look in console and figure out how to map the array of items to an array of objects with: title, tescription, link, thumb
           // 2. Expose this array on the controller (ctrl.videos)
           // 3. Complete the unit test that verifies this structure
           // 4. Adapt the view to display this list (use .video css class for each video div)
---
+++ samples/05Services/scripts/events.js
@@ -0,0 +1,22 @@
+(function () {
+  angular.module('events', [])
+  
+  .controller('AController', function ($rootScope) {
+    var ctrl = this;
+    ctrl.sendMessage = function (message) {
+      $rootScope.$emit('msg', message);
+    };
+  })
+  
+  .controller('BController', function ($rootScope) {
+    var ctrl = this;
+    ctrl.message = '';
+    $rootScope.$on('msg', function (e, msg) {
+      ctrl.message = msg;
+    });
+  });
+})();
+
+// events should be used sparingly. If there are a lot of events in a system, it can be difficult to reason about what is going on
+// services another way of communicating between controllers is to use a service that both controllers depend on.
+// broadcast goes up, emit goes down. 
